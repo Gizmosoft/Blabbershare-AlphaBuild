8b4baf8fd915f9370cd99ebafedeb9aec61c4d41
Gizmosoft <Kartikey.hebbar@gmail.com>
2022-08-24 11:11:42 +0530

    updated dashboard

--- BlabberShare/postapp/models.py
+++ BlabberShare/postapp/models.py
@@ -5,13 +5,16 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
 
+from BlabberShare import settings
+
 
 # Create your models here.
 
 class Post(models.Model):
-    post_id     = models.AutoField(primary_key=True)
+    # post_id     = models.AutoField(primary_key=True)
     post        = models.TextField(max_length=20000)
     image       = models.ImageField(upload_to='images', blank=True)
     hashtags    = models.TextField(blank=True)
     date_time   = models.DateTimeField(default=timezone.now)
+    author      = models.ForeignKey(User, on_delete=models.CASCADE)
 
